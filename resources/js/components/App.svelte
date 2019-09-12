488fcf2f8c9f6e221e288f99510da2ee38068937
sutin <tony.sutin1234@gmail.com>
2019-09-12 09:41:02 +0700

    add new project

---
+++ resources/js/components/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount } from "svelte";
+
+	onMount(() => {
+	  console.log("the component has mounted With Svelte");
+	});
+</script>
+
+<main>
+	<div class="container" style="margin-top: 100px;">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header">
+					 	<h2>Svelte Component</h2>
+					 </div>
+					<div class="card-body">
+					<h4>
+						I'm an example component from Svelte.
+					</h4>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
---
+++ resources/views/svelte.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Laravel Svelte</title>
+
+</head>
+<body>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
+
